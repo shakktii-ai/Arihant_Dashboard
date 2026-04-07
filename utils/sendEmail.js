b5709205c3644245631570463d010360303b89f5
shakktii-ai <shakktii.ai@gmail.com>
2026-04-07 15:02:19 +0530

    update

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -8,8 +8,6 @@ const sendEmail = async (recipientEmail, subject, htmlContent) => {
     port: 587,
     secure: false, // TLS/SSL will be used instead
     auth: {
-      // user: "a72d36001@smtp-brevo.com", // Your SMTP login
-      // pass: "xsmtpsib-d08e1f8956ff9d2d737b7f85a3a506b53525735e7325d9d417aa857326e341c2-NWiga4t1PkRLDvga",         // Your SMTP password (Master Password)
        user: process.env.BREVO_USER, // Use environment variable for SMTP login
       pass: process.env.BREVO_PASS, // Use environment variable for SMTP password
     },
